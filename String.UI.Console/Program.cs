a55275ca6d804ac4cbc3bc9da8420a01df295a9a
DiegochaGit <diegochavez287549@correo.itm.edu.co>
2023-03-21 21:18:58 -0500

    Strings Beam Finished

---
+++ String.UI.Console/Program.cs
@@ -0,0 +1,70 @@
+﻿using Strings.Logic;
+using System;
+
+var myBeams = new MyString[]
+{
+    new MyString("%"),                               //b=t, c=t, w=0, r=t
+    new MyString("%=*"),                             //b=t, c=t, w=3, r=t
+    new MyString("%==="),                            //b=t, c=t, w=6, r=t
+    new MyString("%===*"),                           //b=t, c=t, w=18, r=f
+    new MyString("&===*"),                           //b=t, c=t, w=18, r=t
+    new MyString("&===*==*="),                       //b=t, c=t, w=28, r=t
+    new MyString("#===========*"),                   //b=t, c=t, w=198, r=f
+    new MyString("#====*====================*=="),   //b=t, c=t, w=663, r=f
+    new MyString("&====*====================*=========*======*=====*===="), //b=t, c=t, w=913, r=f
+    new MyString("#==**=="),                         //b=t, c=f, w=12, r=t
+    new MyString("#====**=====*"),                   //b=t, c=f, w=75, r=t
+    new MyString("&=====**====**======"),           //b=t, c=f, w=96, r=f
+    new MyString("*==*"),                            //b=f, c=t, w=9, r=t
+    new MyString("$===*"),                           //b=f, c=t, w=18, r=t
+    new MyString("*===*==*="),                       //b=f, c=t, w=28, r=t
+    new MyString("$===*==*="),                       //b=f, c=t, w=28, r=t
+    new MyString("*===*==*==="),                     //b=f, c=t, w=33, r=t
+};
+
+foreach (var myBeam in myBeams)
+{
+    Console.WriteLine("\n___________________________________");
+    Console.Write("\nThe beam is: ");
+    Console.WriteLine(myBeam.PrintString(myBeam));
+
+    if (myBeam.ValidateBase())
+    {
+        Console.Write("\nThe beam has a valid base");
+    }
+    else
+    {
+        Console.Write("\nThe beam hasn't a valid base");
+    }
+
+    if (myBeam.ValidateConnections())
+    {
+        Console.Write("\nThe beam has valid connections");
+    }
+    else
+    {
+        Console.Write("\nThe beam hasn't valid connections");
+    }
+
+    Console.Write("\nThe beam has a weight of ");
+    Console.Write(myBeam.CalculateWeightBeam());
+
+    if ((myBeam.ValidateBase() && myBeam.ValidateConnections()))
+    {
+        if (myBeam.beamResists(myBeam.CalculateWeightBeam()))
+        {
+            Console.WriteLine("\nThe beam resist the weight");
+        }
+        else
+        {
+            Console.Write("\nThe beam hasn't resist the weight");
+        }
+    } 
+    else
+    {
+        Console.WriteLine("\nWrong beam! It is not calculated if it resists or not");
+    }
+    
+ }
+
+
---
+++ Strings.Logic/MyString.cs
@@ -0,0 +1,91 @@
+﻿namespace Strings.Logic
+{
+    public class MyString
+    {
+        private readonly string _text;
+
+        public MyString(string text)
+        {
+            _text = text;
+        }
+
+       public string ReadBeam()
+        {
+            string myBeam = Console.ReadLine();
+            return myBeam;
+        }
+
+        public bool ValidateBase()
+        {
+            char baseBeam = _text[0];
+            return (baseBeam == '%' || baseBeam == '&' || baseBeam == '#');
+        }
+
+        public bool ValidateConnections()
+        {
+            string aux = _text;
+            for (int i = 0; i < aux.Length - 1; i++)
+            {
+                if (aux[i] == '*')
+                {
+                    if (aux[i] == '*' && aux[i+1] == '*')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public int CalculateWeightBeam()
+        {
+            string aux = _text;
+            int weight = 0;
+            int crossbarCount = 0;
+            for (int i = 1; i < aux.Length; i++)
+            {
+                if (aux[i] == '=')
+                {
+                    crossbarCount++;
+                    weight += crossbarCount;
+                }
+                if (aux[i] == '*')
+                {
+                    weight += 2 * (crossbarCount * (crossbarCount + 1)/2);
+                    crossbarCount = 0;
+                }
+            }
+            return weight;
+        }
+
+        public bool beamResists(int weight)
+        {
+            char baseBeam = _text[0];
+            if (baseBeam == '%' && weight > 10)
+            {
+                return false;
+            }
+            if (baseBeam == '&' && weight > 30)
+            {
+                return false;
+            }
+            if (baseBeam == '#' && weight > 90)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public string? PrintString(MyString myBeam)
+        {
+            string output = string.Empty;
+            for (int i = 0; i < _text.Length; i++)
+            {
+                output += $"{_text[i]}";
+            }
+            return output;
+        }
+
+    }
+}
+
